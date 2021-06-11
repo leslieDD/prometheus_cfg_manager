47a87ccbe5ce34916975dcd389948863dfb62b75
liaohg <liaohg@666id.com>
2021-06-11 10:55:56 +0800

    update update.sh

--- update.sh
+++ update.sh
@@ -2,4 +2,6 @@
 go build
 rm -rf /usr/local/prometheus_cfg_manager/pro_cfg_manager
 cp pro_cfg_manager /usr/local/prometheus_cfg_manager
+rm -rf /usr/local/prometheus_cfg_manager/static
+cp -rf ./static /usr/local/prometheus_cfg_manager/
 systemctl restart pro_cfg_manager
