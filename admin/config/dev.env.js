36fe8535d4146dd93c75d389bdf78b9335f8879f
liaohg <liaohg@666id.com>
2021-06-11 19:27:19 +0800

    配置多开发环境

--- admin/config/dev.env.js
+++ admin/config/dev.env.js
@@ -2,6 +2,11 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
-})
+if (process.env.NODE_ENV === 'development') {
+  module.exports = merge(prodEnv, {
+    NODE_ENV: '"development"',
+    VUE_APP_BASE_API: '"http://127.0.0.1:8200"'
+  })
+} else {
+  module.exports = prodEnv
+}
