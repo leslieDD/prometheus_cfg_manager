2c03b74242a0cbfefae149275efb12afa4f137e5
liaohg <liaohg@666id.com>
2021-08-06 17:53:47 +0800

    vite编辑

---
+++ static/assets/login.3f821e0a.js
@@ -0,0 +1,1 @@
+import{s as t}from"./request.c24bac87.js";function r(r){return t({url:"/login",method:"post",data:r})}function o(r){return t({url:"/register",method:"post",data:r})}export{r as l,o as r};
--- static/assets/srv.929685a5.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"./request.0c690da8.js";function o(){return r({url:"/v1/reload",method:"post"})}export{o as r};
