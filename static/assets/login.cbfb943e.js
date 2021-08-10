feaaeb6e7e0ec2ba1315b3df82fe6619019023c8
liaohg <liaohg@666id.com>
2021-08-10 17:20:26 +0800

    vite编译

--- static/assets/login.cbfb943e.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./request.f1ddaf6e.js";function r(r){return t({url:"/login",method:"post",data:r})}function e(r){return t({url:"/register",method:"post",data:r})}export{r as l,e as r};
