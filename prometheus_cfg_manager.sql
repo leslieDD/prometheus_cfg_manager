06c27cd98bd2771115d15ffacfde0e4f68ca25a3
liaohg <liaohg@666id.com>
2021-08-10 17:10:30 +0800

    add config and update sql

--- prometheus_cfg_manager.sql
+++ prometheus_cfg_manager.sql
@@ -14,12 +14,10 @@
 
 
 -- 导出 pro_cfg_manager 的数据库结构
-DROP DATABASE IF EXISTS `pro_cfg_manager`;
 CREATE DATABASE IF NOT EXISTS `pro_cfg_manager` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `pro_cfg_manager`;
 
 -- 导出  表 pro_cfg_manager.annotations 结构
-DROP TABLE IF EXISTS `annotations`;
 CREATE TABLE IF NOT EXISTS `annotations` (
   `id` int NOT NULL AUTO_INCREMENT,
   `monitor_rules_id` int NOT NULL,
@@ -32,7 +30,6 @@ CREATE TABLE IF NOT EXISTS `annotations` (
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.group_labels 结构
-DROP TABLE IF EXISTS `group_labels`;
 CREATE TABLE IF NOT EXISTS `group_labels` (
   `id` int NOT NULL AUTO_INCREMENT,
   `job_group_id` int NOT NULL,
@@ -40,6 +37,7 @@ CREATE TABLE IF NOT EXISTS `group_labels` (
   `value` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `enabled` tinyint NOT NULL DEFAULT '1',
   `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_by` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_groupid_key` (`key`,`job_group_id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='子组标签列表';
@@ -47,7 +45,6 @@ CREATE TABLE IF NOT EXISTS `group_labels` (
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.group_machines 结构
-DROP TABLE IF EXISTS `group_machines`;
 CREATE TABLE IF NOT EXISTS `group_machines` (
   `id` int NOT NULL AUTO_INCREMENT,
   `job_group_id` int NOT NULL,
@@ -60,18 +57,16 @@ CREATE TABLE IF NOT EXISTS `group_machines` (
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.group_priv 结构
-DROP TABLE IF EXISTS `group_priv`;
 CREATE TABLE IF NOT EXISTS `group_priv` (
   `id` int NOT NULL AUTO_INCREMENT,
   `group_id` int NOT NULL,
   `func_id` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3240 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3626 DEFAULT CHARSET=utf8;
 
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.jobs 结构
-DROP TABLE IF EXISTS `jobs`;
 CREATE TABLE IF NOT EXISTS `jobs` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
   `name` varchar(100) NOT NULL COMMENT '任务名称',
@@ -82,6 +77,7 @@ CREATE TABLE IF NOT EXISTS `jobs` (
   `display_order` int unsigned NOT NULL,
   `enabled` tinyint NOT NULL DEFAULT '1',
   `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_by` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_unique` (`name`),
   KEY `order_unique` (`display_order`)
@@ -90,20 +86,19 @@ CREATE TABLE IF NOT EXISTS `jobs` (
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.job_group 结构
-DROP TABLE IF EXISTS `job_group`;
 CREATE TABLE IF NOT EXISTS `job_group` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `jobs_id` int NOT NULL,
   `enabled` tinyint NOT NULL DEFAULT '1',
   `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_by` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='还可以为单个分组中的IP地址进行分子组，为每个子组设置相应的标签列表';
 
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.job_machines 结构
-DROP TABLE IF EXISTS `job_machines`;
 CREATE TABLE IF NOT EXISTS `job_machines` (
   `machine_id` int NOT NULL,
   `job_id` int NOT NULL,
@@ -113,38 +108,49 @@ CREATE TABLE IF NOT EXISTS `job_machines` (
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.labels 结构
-DROP TABLE IF EXISTS `labels`;
 CREATE TABLE IF NOT EXISTS `labels` (
   `id` int NOT NULL AUTO_INCREMENT,
   `label` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `enabled` tinyint NOT NULL DEFAULT '1',
   `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_by` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_labels` (`label`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='标签列表';
 
 -- 数据导出被取消选择。
 
+-- 导出  表 pro_cfg_manager.log_setting 结构
+CREATE TABLE IF NOT EXISTS `log_setting` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `level` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `label` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `selected` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COMMENT='日志设置';
+
+-- 数据导出被取消选择。
+
 -- 导出  表 pro_cfg_manager.machines 结构
-DROP TABLE IF EXISTS `machines`;
 CREATE TABLE IF NOT EXISTS `machines` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
   `ipaddr` varchar(100) NOT NULL COMMENT 'IP地址',
   `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `enabled` tinyint NOT NULL DEFAULT '1',
+  `update_by` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `ipaddr_unique` (`ipaddr`)
-) ENGINE=InnoDB AUTO_INCREMENT=184 DEFAULT CHARSET=utf8 COMMENT='机器列表';
+) ENGINE=InnoDB AUTO_INCREMENT=397 DEFAULT CHARSET=utf8 COMMENT='机器列表';
 
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.manager_group 结构
-DROP TABLE IF EXISTS `manager_group`;
 CREATE TABLE IF NOT EXISTS `manager_group` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `enabled` tinyint NOT NULL,
   `update_at` datetime NOT NULL,
+  `update_by` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_unique` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='用户组';
@@ -152,7 +158,6 @@ CREATE TABLE IF NOT EXISTS `manager_group` (
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.manager_set 结构
-DROP TABLE IF EXISTS `manager_set`;
 CREATE TABLE IF NOT EXISTS `manager_set` (
   `param_name` varbinary(100) NOT NULL,
   `param_value` varbinary(100) NOT NULL
@@ -161,10 +166,10 @@ CREATE TABLE IF NOT EXISTS `manager_set` (
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.manager_user 结构
-DROP TABLE IF EXISTS `manager_user`;
 CREATE TABLE IF NOT EXISTS `manager_user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL,
+  `nice_name` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `phone` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
   `salt` varchar(100) NOT NULL,
@@ -172,14 +177,14 @@ CREATE TABLE IF NOT EXISTS `manager_user` (
   `update_at` datetime NOT NULL,
   `enabled` tinyint NOT NULL,
   `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_by` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_name` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='用户';
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='用户';
 
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.monitor_labels 结构
-DROP TABLE IF EXISTS `monitor_labels`;
 CREATE TABLE IF NOT EXISTS `monitor_labels` (
   `id` int NOT NULL AUTO_INCREMENT,
   `monitor_rules_id` int NOT NULL,
@@ -192,7 +197,6 @@ CREATE TABLE IF NOT EXISTS `monitor_labels` (
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.monitor_rules 结构
-DROP TABLE IF EXISTS `monitor_rules`;
 CREATE TABLE IF NOT EXISTS `monitor_rules` (
   `id` int NOT NULL AUTO_INCREMENT,
   `alert` varchar(100) NOT NULL,
@@ -201,28 +205,29 @@ CREATE TABLE IF NOT EXISTS `monitor_rules` (
   `sub_group_id` int NOT NULL,
   `enabled` tinyint NOT NULL,
   `description` varchar(300) NOT NULL,
+  `update_by` varchar(100) NOT NULL,
+  `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8 COMMENT='具体的监控规则';
 
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.operation_log 结构
-DROP TABLE IF EXISTS `operation_log`;
 CREATE TABLE IF NOT EXISTS `operation_log` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL,
+  `operate_type` varchar(50) NOT NULL,
   `ipaddr` varchar(100) NOT NULL,
   `operate_name` varchar(100) NOT NULL,
   `operate_result` tinyint NOT NULL,
   `operate_at` datetime NOT NULL,
   `operate_error` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=145 DEFAULT CHARSET=utf8 COMMENT='操作日志';
+) ENGINE=InnoDB AUTO_INCREMENT=148 DEFAULT CHARSET=utf8 COMMENT='操作日志';
 
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.options 结构
-DROP TABLE IF EXISTS `options`;
 CREATE TABLE IF NOT EXISTS `options` (
   `id` int NOT NULL AUTO_INCREMENT,
   `opt_key` varchar(100) NOT NULL,
@@ -233,7 +238,6 @@ CREATE TABLE IF NOT EXISTS `options` (
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.page_function 结构
-DROP TABLE IF EXISTS `page_function`;
 CREATE TABLE IF NOT EXISTS `page_function` (
   `id` int NOT NULL AUTO_INCREMENT,
   `page_name` varchar(100) NOT NULL,
@@ -243,18 +247,18 @@ CREATE TABLE IF NOT EXISTS `page_function` (
   `func_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `func_nice_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=93 DEFAULT CHARSET=utf8 COMMENT='页面功能';
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8 COMMENT='页面功能';
 
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.relabels 结构
-DROP TABLE IF EXISTS `relabels`;
 CREATE TABLE IF NOT EXISTS `relabels` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(200) NOT NULL,
   `code` text CHARACTER SET utf8 COLLATE utf8_general_ci,
   `enabled` tinyint NOT NULL DEFAULT '1',
   `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_by` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COMMENT='重写标签的配置';
@@ -262,10 +266,11 @@ CREATE TABLE IF NOT EXISTS `relabels` (
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.rules_groups 结构
-DROP TABLE IF EXISTS `rules_groups`;
 CREATE TABLE IF NOT EXISTS `rules_groups` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
+  `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_by` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='监控规则组';
@@ -273,7 +278,6 @@ CREATE TABLE IF NOT EXISTS `rules_groups` (
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.session 结构
-DROP TABLE IF EXISTS `session`;
 CREATE TABLE IF NOT EXISTS `session` (
   `id` int NOT NULL AUTO_INCREMENT,
   `token` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
@@ -281,38 +285,58 @@ CREATE TABLE IF NOT EXISTS `session` (
   `update_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_user_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8 COMMENT='用户登录会话';
+) ENGINE=InnoDB AUTO_INCREMENT=94 DEFAULT CHARSET=utf8 COMMENT='用户登录会话';
 
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.sub_group 结构
-DROP TABLE IF EXISTS `sub_group`;
 CREATE TABLE IF NOT EXISTS `sub_group` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `rules_groups_id` int NOT NULL,
+  `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_by` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_name_rules_groups_id` (`name`,`rules_groups_id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='监控规则组中的下级组，子组';
 
 -- 数据导出被取消选择。
 
--- 导出  表 pro_cfg_manager.test 结构
-DROP TABLE IF EXISTS `test`;
-CREATE TABLE IF NOT EXISTS `test` (
+-- 导出  表 pro_cfg_manager.system_log 结构
+CREATE TABLE IF NOT EXISTS `system_log` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `unique` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+  `username` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `operate_type` varchar(50) NOT NULL,
+  `ipaddr` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `operate_content` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `operate_result` tinyint NOT NULL,
+  `operate_at` datetime NOT NULL,
+  `operate_error` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=385 DEFAULT CHARSET=utf8 COMMENT='系统日志';
 
 -- 数据导出被取消选择。
 
 -- 导出  表 pro_cfg_manager.tmpl 结构
-DROP TABLE IF EXISTS `tmpl`;
 CREATE TABLE IF NOT EXISTS `tmpl` (
-  `tmpl` longtext NOT NULL
+  `tmpl` longtext NOT NULL,
+  `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_by` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='prometheus.yml';
+
+-- 数据导出被取消选择。
+
+-- 导出  表 pro_cfg_manager.tmpl_fields 结构
+CREATE TABLE IF NOT EXISTS `tmpl_fields` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `key` varchar(100) NOT NULL,
+  `value` varchar(500) NOT NULL,
+  `enabled` tinyint NOT NULL,
+  `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_by` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `key_unique` (`key`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='模板字段';
 
 -- 数据导出被取消选择。
 
