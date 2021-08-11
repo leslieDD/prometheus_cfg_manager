f559a69380c14df6de3a1820de51e763bba9216b
liaohg <liaohg@666id.com>
2021-08-11 15:57:22 +0800

    vite compile

--- static/assets/login.d5007e49.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./request.10a66ff0.js";function r(r){return t({url:"/login",method:"post",data:r})}function o(r){return t({url:"/register",method:"post",data:r})}export{r as l,o as r};
