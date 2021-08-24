d9818e52052307ba030be4607272e4e961b43284
liaohg <liaohg@fire.com>
2021-08-24 09:00:11 +0800

    mkdir

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 go build
 rm -rf /usr/local/prometheus_cfg_manager/pro_cfg_manager
+mkdir -p /usr/local/prometheus_cfg_manager
 cp pro_cfg_manager /usr/local/prometheus_cfg_manager
 if [ -f /usr/local/prometheus_cfg_manager/config.yml ];then
     mv /usr/local/prometheus_cfg_manager/config.yml /usr/local/prometheus_cfg_manager/config.yml.$(date +"%s")
