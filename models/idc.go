d7a6224bf3832678c1fe70641f72414fd6a0c8ae
diandian <diandian@dd.com>
2022-03-21 14:49:09 +0800

    修复时间错误

--- models/idc.go
+++ models/idc.go
@@ -287,7 +287,7 @@ func PutLineIpAddrs(user *UserSessionInfo, newPool *NewPool) *BriefMessage {
 		return ErrDataBase
 	}
 	sql := fmt.Sprintf("replace into pool(line_id, ipaddrs, remark, update_at, update_by) values(%d, '%s', '%s', '%s', '%s');",
-		newPool.LineID, newPool.Ipaddrs, newPool.Remark, time.Now().Format("2006-02-01 15:04:05"), user.Username)
+		newPool.LineID, newPool.Ipaddrs, newPool.Remark, time.Now().Format("2006-01-02 15:04:05"), user.Username)
 	tx := db.Table("pool").Exec(sql)
 	if tx.Error != nil {
 		config.Log.Error(tx.Error)
