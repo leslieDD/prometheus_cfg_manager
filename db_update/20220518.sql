1a3c8f883266f1b3d2381b760c816897f7826197
DianDian <leslie@dd.com>
2022-05-18 12:10:43 +0800

    right to top

---
+++ db_update/20220518.sql
@@ -0,0 +1,6 @@
+insert options(id, opt_key, opt_value)
+values(Null, 'group_by_idc_line_label', 'true');
+insert options(id, opt_key, opt_value)
+values(Null, 'labeled_ip_no_label', 'true');
+insert options(id, opt_key, opt_value)
+values(Null, 'labeled_all_ip', 'false');
