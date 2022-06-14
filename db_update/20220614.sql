a4bc5571b496d2b3d04c4ddf13699c56af6ff596
DianDian <leslie@dd.com>
2022-06-14 17:25:35 +0800

    update

---
+++ db_update/20220614.sql
@@ -0,0 +1,16 @@
+INSERT INTO `pro_cfg_manager`.`page_function` (
+    `page_name`,
+    `page_nice_name`,
+    `sub_page_name`,
+    `sub_page_nice_name`,
+    `func_name`,
+    `func_nice_name`
+  )
+VALUES (
+    'person',
+    '个人中心',
+    '',
+    '',
+    'show_menu_hls',
+    '直播'
+  );
